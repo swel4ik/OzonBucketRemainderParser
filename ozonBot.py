3dddc1251198b28bad033d510a2478880def0d99
Dmitry Zadorozhniy <zadorojniy.sanya@yandex.ru>
2022-10-12 23:39:41 +0300

    added stealth

--- ozonBot.py
+++ ozonBot.py
@@ -3,10 +3,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium_stealth import stealth
 import datetime
 import re
 import os
 import json
+import time
 
 if __name__ == '__main__':
     with open('config.json') as config:
@@ -14,6 +16,8 @@ if __name__ == '__main__':
 
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
                          "like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.67 (Edition Yx 05)")
     options.add_argument(f'--user-data-dir={config_data["user-data-dir"]}')
@@ -22,14 +26,23 @@ if __name__ == '__main__':
     s = Service(r"chromedriver\chromedriver.exe")
     driver = webdriver.Chrome(service=s,
                               options=options)
+    stealth(driver,
+            languages=["ru-RU", "ru"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
 
-    if not os.path.exists('ozon_data.csv'):
-        with open('ozon_data.csv', 'w') as f:
+    if not os.path.exists((os.path.join(config_data['csv_save_path'], 'ozon_data.csv'))):
+        with open((os.path.join(config_data['csv_save_path'], 'ozon_data.csv')), 'w') as f:
             f.write('Name|Colour|Seller|Remainder|Date\n')
 
-    driver.get("https://www.ozon.ru/")
-    bucket = driver.find_element(By.XPATH, '//*[@id="stickyHeader"]/div[4]/a[2]/span[2]')
-    bucket.click()
+    driver.get("https://www.ozon.ru/cart")
+    # bucket = driver.find_element(By.XPATH, '//*[@id="stickyHeader"]/div[4]/a[2]/span[2]')
+    # time.sleep(4)
+    # bucket.click()
     positions_amount = len(driver.find_elements(By.XPATH,
                                                 '//*[@id="layoutPage"]/div[1]/div/div/div[3]/div[4]/div[1]/div['
                                                 '1]/div/div[2]/div'))
@@ -54,10 +67,11 @@ if __name__ == '__main__':
         amount.click()
         ost = -1
         try:
-            WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH,
+            WebDriverWait(driver, 2.5).until(EC.element_to_be_clickable((By.XPATH,
                                                                        "//*[contains(text(), '10+')]"))).click()
         except:
             ost = '<10'
+            amount.click()
 
         if ost != -1:
             left = '<10'
